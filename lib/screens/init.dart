844a4a56800c0902efbbb32701402968ef6b0b04
CodeJunior06 <san1428.srp@gmail.com>
2022-06-21 16:00:12 -0500

    Splash screen native

--- lib/screens/init.dart
+++ lib/screens/init.dart
@@ -1,4 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:movie_consumer/screens/main.dart';
 
-void main() => runApp(const Main());
+void main(){
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(const Main());
+} 
